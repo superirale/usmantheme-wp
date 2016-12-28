05fd4941fb9213be42ccaf02a1117cc33565bc55
Usman Irale <superirale@gmail.com>
2016-12-28 18:42:08 +0100

    style.css changed

--- posts.php
+++ posts.php
@@ -4,7 +4,7 @@
           <?php if(have_posts()):?>
             <?php while(have_posts()): the_post();?>
               <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
-              <div class="post-date"><i class="fa fa-custom fa-clock-o"></i> <span>Dec 22, 2016</span></div>
+              <div class="post-date"><i class="fa fa-custom fa-clock-o"></i> <span><?php the_date('M d, Y'); ?></span></div>
 
               <?php the_content(); ?>
 
---
+++ single.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+     <div class="blog-post">
+     <?php while ( have_posts() ) : the_post();?>
+        <div class="post-header">
+            <h1><?php the_title();?></h1>
+            <div class="post-date">
+              <i class="fa fa-custom fa-clock-o"></i>
+              <span><?php the_date('M d, Y'); ?></span></div>
+        </div>
+        <div class="post-body">
+            <p><?php the_content(); ?></p>
+        <hr>
+
+        </div>
+      <?php endwhile; ?>
+        <p class="social-share">
+            Please share, if you find this article helpful.
+            <br><br>
+
+            <a class="twitter-share-button"
+              href="https://twitter.com/intent/tweet?text=Introduction to Flask"
+              data-size="large"><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+            Tweet</a>
+        </p>
+
+
+  <?php get_footer();?>
